1eecb2227ebbd3ce8ca1a4fbe6cf58f2144c4a61
Charles Condon <charlescon@live.com>
2025-10-04 22:22:07 -0500

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rye } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
+const rye = Rye({
+    weight: "400",
     subsets: ["latin"],
 });
 
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
+            <body className={`${rye.className} antialiased`}>
                 <Navbar />
                 {children}
             </body>
